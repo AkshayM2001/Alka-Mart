e2f019efbdb1d59b8aaa45a76c3fd9d3029ae281
AkshayM2001 <akshaymatale2001@gmail.com>
2024-02-20 18:32:01 +0530

    password change done page not working

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name="logout"),
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
-    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm), name='passwordchange'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='passwordchange'),
     path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
     # path('registration/', views.customerregistration, name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
