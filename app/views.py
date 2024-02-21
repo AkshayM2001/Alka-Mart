abb2a9abccafc602fb2674a6fc9f58069f01ba34
AkshayM2001 <akshaymatale2001@gmail.com>
2024-02-21 20:50:48 +0530

    authentication complete startes profile

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Customer,Product,Cart,OrderPlaced
-from .forms import CustomerRegistrationForm
+from .forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 # def home(request):
 #  return render(request, 'app/home.html')
@@ -33,8 +33,8 @@ def add_to_cart(request):
 def buy_now(request):
  return render(request, 'app/buynow.html')
 
-def profile(request):
- return render(request, 'app/profile.html')
+# def profile(request):
+#  return render(request, 'app/profile.html')
 
 def address(request):
  return render(request, 'app/address.html')
@@ -78,3 +78,23 @@ class CustomerRegistrationView(View):
 
 def checkout(request):
  return render(request, 'app/checkout.html')
+
+
+class ProfileView(View):
+ def get(self, request):
+  form = CustomerProfileForm()
+  return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary'})
+ 
+ def post(self, request):
+  form = CustomerProfileForm(request.POST)
+  if form.is_valid():
+   usr = request.user
+   name = form.cleaned_data['name']
+   locality = form.cleaned_data['locality']
+   city = form.cleaned_data['city']
+   zipcode = form.cleaned_data['zipcode']
+   state = form.cleaned_data['state']
+   reg = Customer(user=usr, name=name, locality=locality, city=city, zipcode=zipcode, state=state)
+   reg.save()
+   messages.success(request, 'Congratulations!! profile updated successfully!')
+  return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary'})
