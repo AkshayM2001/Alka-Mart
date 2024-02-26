b1a10e10964d416066cd31137797ef2b42845e97
AkshayM2001 <akshaymatale2001@gmail.com>
2024-02-26 13:02:51 +0530

    render 2.0

--- Shopsite/settings.py
+++ Shopsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#nrs1u(be6&y@8&w%#b%6j*jfc2n)@i+(&8w#^qt!z%fe7_=c2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'Shopsite',
 ]
 
 MIDDLEWARE = [
